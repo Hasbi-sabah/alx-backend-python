32d5dd6fa038bf6a5db252faae486667676d6e66
H. Sabah <sabahhasbi00@gmail.com>
2024-01-16 10:07:28 +0000

    Code fix: module level import at top of file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" modult for the coroutine async_comprehension """
-async_generator = __import__("0-async_generator").async_generator
-
+""" module for the coroutine async_comprehension """
 from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
