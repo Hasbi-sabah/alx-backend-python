d7c4c812d6c2a7ca80f1849bf34e11f401a3c1dc
H. Sabah <sabahhasbi00@gmail.com>
2024-02-02 10:27:56 +0000

    Update: type annotation

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@
 from unittest import TestCase
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
 class TestGithubOrgClient(TestCase):
@@ -12,7 +12,7 @@ class TestGithubOrgClient(TestCase):
 
     @parameterized.expand(["google", "abc"])
     @patch("client.get_json")
-    def test_org(self, org_name, mocked_get_json):
+    def test_org(self, org_name: str, mocked_get_json: MagicMock):
         """Test the org method of GithubOrgClient."""
         GithubOrgClient(org_name).org
         mocked_get_json.assert_called_once_with(
